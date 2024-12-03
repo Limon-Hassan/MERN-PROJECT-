0029ca27459da22066f503a0760b1ad0589e11a3
Mahammud Hassan Limon <mahammudhassanlimon@gmail.com>
2024-12-04 02:31:50 +0600

    benner done

--- src/Components/A.Navber/Navber.jsx
+++ src/Components/A.Navber/Navber.jsx
@@ -5,7 +5,7 @@ import Benner from "../Benner/Benner";
 const Navber = () => {
   return (
     <>
-      <nav className="bg-black py-[0px]">
+      <nav className="bg-[#000080] opacity-80 py-[0px]">
         <Container>
           <div className="main flex justify-between items-center">
             <a href="#">
@@ -17,16 +17,16 @@ const Navber = () => {
             </a>
             <div className="flex gap-[80px] items-center">
               <ul className="flex">
-                <li className="text-[20px] font-robot-h1 font-medium text-[#123456] ml-5 hover:text-[#FFF] ease-in-out duration-300">
+                <li className="text-[20px] font-robot-h1 font-medium text-[#1E90FF] ml-5 hover:text-[#FFF] ease-in-out duration-300">
                   <a href="#">About us</a>
                 </li>
-                <li className="text-[20px] font-robot-h1 font-medium text-[#123456] ml-5 hover:text-[#FFF] ease-in-out duration-300">
+                <li className="text-[20px] font-robot-h1 font-medium text-[#1E90FF] ml-5 hover:text-[#FFF] ease-in-out duration-300">
                   <a href="#">Contact us</a>
                 </li>
               </ul>
               <button
                 className="text-[20px] font-for-all font-medium text-[#FFf] border border-[#FFF]
-              px-[30px] py-[12px]"
+              px-[30px] py-[12px] hover:bg-white hover:text-[#000080] ease-in-out duration-300"
               >
                 Learn mern
               </button>
--- src/Components/Benner/Benner.jsx
+++ src/Components/Benner/Benner.jsx
@@ -1,27 +1,56 @@
 import Container from "../container/Container";
-import { Cursor, useTypewriter } from "react-simple-typewriter";
+import { Typewriter } from "react-simple-typewriter";
 
 const Benner = () => {
-  const { text } = useTypewriter({
-    words: ["MERN STACK DEVELOPERS", "MERN STACK DEVELOPERS"],
-    loop: {},
-    typeSpeed: 120,
-    deleteSpeed: 80,
-  });
   return (
     <>
-      <section className="bg-[url('/sobai.jpg')] w-full h-full">
-        <Container>
-          <div className="text-center">
-            <h1>
-              We Are {""}
-              <span>{text}</span>
-              <span>
-                <Cursor cursorStyle="|" />
-              </span>
-            </h1>
-          </div>
-        </Container>
+      <section className="bg-[url('/sobai.jpg')]  w-full h-full relative bg-no-repeat bg-cover ">
+        <img
+          className=" w-[1000px] h-[500px] absolute top-[230px] left-[100px] z-10"
+          src="/com_2.png"
+          alt="com_1"
+        />
+        <img
+          className=" w-[900px] h-[500px] absolute top-[150px] right-0 z-10"
+          src="/file.png"
+          alt="com_1"
+        />
+        <div className="backdrop-blur-[12px] bg-blue-800/50 w-full h-screen">
+          <Container>
+            <div className="text-center">
+              <div className="flex mx-auto justify-center items-center pt-[180px] mb-5">
+                <div>
+                  <h1 className="text-[30px] font-popin font-bold text-white mx-auto uppercase tracking-wide">
+                    We Are
+                  </h1>
+                </div>
+                <span className="text-[30px] font-popin font-bold text-white w-[12%] tracking-wide">
+                  <Typewriter
+                    words={["MERN  ", "MERN  ", "MERN  ", "MERN  "]}
+                    loop={""}
+                    typeSpeed={200}
+                    deleteSpeed={100}
+                    delaySpeed={1000}
+                  />
+                </span>
+                <h1 className="text-[30px] font-popin font-bold text-white uppercase tracking-wide">
+                  Stack Developers
+                </h1>
+              </div>
+              <p className="text-[18px] font-normal font-for-p text-white w-[400px] mx-auto leading-8">
+                <span className="text-[22px] font-for-p font-semibold text-white hover:border-b hover:border-blue-50 leading-6">
+                  since
+                </span>{" "}
+                in 28 December 2023 with a specialist Mentor wasim Mahmood Reja
+                sir we started to learn . we learning at most wonderful IT
+                industry it is Creative IT institute. Its south asian most
+                awarded Industry. From there we got 24 hours support and lifetime
+                support . They have career placement department (CPD) , most
+                valuable part of creative it institute
+              </p>
+            </div>
+          </Container>
+        </div>
       </section>
     </>
   );
