34445648e8b35215be035cbfabe90c6daa8b6bef
Mahammud Hassan Limon <mahammudhassanlimon@gmail.com>
2024-12-07 04:54:55 +0600

    second drop

--- src/Components/Benner_3/Benner_3.jsx
+++ src/Components/Benner_3/Benner_3.jsx
@@ -1,13 +1,17 @@
-import React from 'react'
+import React from "react";
+import Container from "../container/Container";
 
 const Benner_3 = () => {
+  
   return (
     <>
       <section className="bg-blue-800 pb-[200px]">
-        
+        <Container>
+          
+        </Container>
       </section>
     </>
   );
-}
+};
 
-export default Benner_3
+export default Benner_3;
